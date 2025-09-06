afcd8523f03468f5c38d60e2b5aa2f342fb9f78b
Scooter1974 <talldarkandscooter@gmail.com>
2025-09-06 16:35:01 -0300

    Added "Berry Proofed" advancement

--- src/main/java/com/scooter1974/berryproof/BerryProofConfigMidnight.java
+++ src/main/java/com/scooter1974/berryproof/BerryProofConfigMidnight.java
@@ -5,7 +5,7 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class BerryProofConfigMidnight extends MidnightConfig {
     public static final String GENERAL = "general";
 
-    @Entry(category = GENERAL, name = "Enable sound when damage is negated")
+    @Entry(category = GENERAL, name = "Enable sound when damage is blocked")
     public static boolean enableSound = true;
 
     @Entry(category = GENERAL, name = "Sound volume (0.0 to 1.0)", min = 0.0, max = 1.0, isSlider = true)
